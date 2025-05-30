ca392697d9528ac3d74d1fe82682909c9481f4f6
haseenadilruba <rdeepthi154@gmail.com>
2025-05-30 19:19:43 +0530

    Create user_login.php

---
+++ user_login.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include('db.php'); // Connects to database
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email='$email' AND role='user'";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        header("Location: dashboard_user.php"); // Redirect to User Dashboard
+        exit();
+    } else {
+        echo "<script>alert('Invalid email or password'); window.location.href='userlogin.html';</script>";
+    }
+}
+?>
